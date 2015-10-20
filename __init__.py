eb5d1d1dd8ec7a0ffe1a07969df1c83b570d6485
Hunter John Larco <admin@larcolabs.com>
2015-10-19 22:06:03 -0700

    db console friendly
    moved inspector import so that flask libraries are not necessary unless
    running the inspection server

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from . import inspector
-
 from .key import Key
 from .model import Model
 from .properties import *
 
 
 def start_development_server(port=8000, debug=False, threading=True):
+  from . import inspector
+  # placed here so that when a flask server isn't running one can still use the db library
   if threading:
     from threading import Thread
     Thread(target=inspector.run, args=(port,debug)).start()
